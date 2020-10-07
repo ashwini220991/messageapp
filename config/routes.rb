86b1cbae908bfb98a5f024f4b69301960dfa1e44
ashwini <ashwini05rahul@gmail.com>
2020-10-07 22:33:59 +0530

    messages added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'sessions#new'
+  root 'chatroom#index'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
